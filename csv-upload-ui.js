d68fc5769bcad409c42fbb4940e7241ed101c4ee
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-16 16:40:37 +0200

    Initial.

---
+++ csv-upload-ui.js
@@ -0,0 +1,14 @@
+if (Meteor.isClient) {
+  //
+  });
+
+  Template.upload.events({
+   //
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
