873ad60bff8c34b8306af274c587a114de3b9c15
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-16 22:18:25 +0200

    Basic working example.

--- csv-upload-ui.js
+++ csv-upload-ui.js
@@ -3,7 +3,6 @@ Items = new Mongo.Collection('items');
 if (Meteor.isClient) {
     Template.upload.events({
         'change [name=file]': function (event, template) {
-            console.log("Changed!");
             try {
                 // Get the file from the form
                 csvFile = event.target.files[0];
@@ -11,6 +10,10 @@ if (Meteor.isClient) {
                 // If file not available
                 console.log("Could not access file.");
             }
+
+            // clear the file selector input
+            event.target.value = "";
+
             // Parse the file to an object
             Papa.parse(csvFile, {
                 header: true,
@@ -19,9 +22,11 @@ if (Meteor.isClient) {
                     // get the data
                     data = results.data;
 
-                    // Insert each datum into collection
+                    // Insert each record into collection
                     _.each(data, function (datum) {
-                        Items.insert(datum);
+                        record = datum;
+                        record.dateCreated = new Date();
+                        Items.insert(record);
                     });
                 }
             });
@@ -29,15 +34,43 @@ if (Meteor.isClient) {
     });
     Template.display.helpers({
         'items': function () {
-            return Items.find({});
+            // return all documents
+            // reverse-sorted by date created
+            return Items.find();
+        }
+    });
+    Template.item.helpers({
+        'itemKeyValue': function () {
+            // This function maps the key/value pairs for a given document
+            // to object attributes called 'key' and 'value'
+            // so that the values can be accessed in the template by {{key}} and {{value}}
+
+
+            // Create empty array
+            var itemKeyValue = [];
+
+            // counter variable
+            var index = 0;
+
+            // Find the active document
+            var itemDoc = Items.findOne(this._id);
+
+            // Iterate through each key/value pair in the document
+            _(itemDoc).each( function( value, key, itemDoc ) {
+                // Create an empty object at the index
+                itemKeyValue[index] = {};
+
+                // Set the key value to key
+                itemKeyValue[index]['key'] = key;
+
+                // Set the value value to value
+                itemKeyValue[index]['value'] = value;
+
+                // increment the counter
+                index++;
+            });
+            console.log(itemKeyValue);
+            return itemKeyValue;
         }
     });
 };
-
-
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // code to run on server at startup
-    });
-}
