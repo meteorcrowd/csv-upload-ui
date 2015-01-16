be4be7909232384232e0dc4e160022e60420f4ba
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-16 18:03:57 +0200

    Basic working example.

--- csv-upload-ui.js
+++ csv-upload-ui.js
@@ -1,14 +1,43 @@
-if (Meteor.isClient) {
-  //
-  });
+Items = new Mongo.Collection('items');
 
-  Template.upload.events({
-   //
-  });
-}
+if (Meteor.isClient) {
+    Template.upload.events({
+        'change [name=file]': function (event, template) {
+            console.log("Changed!");
+            try {
+                // Get the file from the form
+                csvFile = event.target.files[0];
+            } catch (e) {
+                // If file not available
+                console.log("Could not access file.");
+            }
+            // Parse the file to an object
+            Papa.parse(csvFile, {
+                header: true,
+                dynamicTyping: true,
+                complete: function (results) {
+                    // get the data
+                    data = results.data;
+
+                    // Insert each datum into collection
+                    _.each(data, function (datum) {
+                        Items.insert(datum);
+                    });
+                }
+            });
+        }
+    });
+    Template.display.helpers({
+        'items': function () {
+            return Items.find({});
+        }
+    });
+};
+
+
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
